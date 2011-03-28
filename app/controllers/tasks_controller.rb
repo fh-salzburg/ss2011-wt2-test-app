bc45ab945b01f11a1ca442536d8f2a1d7a473b29
Michael Raidel <raidel@induktiv.at>
2011-03-28 13:30:21 +0200

    getting tests to pass again

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,18 @@ class TasksController < ApplicationController
   def index
    @tasks = Task.all
   end
+
+  def new 
+    @task = Task.new
+  end 
+
+  def create
+    @task = Task.new(params[:task])
+
+    if @task.save
+      redirect_to tasks_path, :notice => "Task erfolgreich angelegt"
+    else
+      render 'new'
+    end 
+  end
 end
