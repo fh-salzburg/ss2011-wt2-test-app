ca2fb3be5664fa2db7650fcf70ed4deecf33315a
Michael Raidel <raidel@induktiv.at>
2011-03-28 12:55:35 +0200

    remove fixtures and stub test

--- test/integration/tasks_integration_test.rb
+++ test/integration/tasks_integration_test.rb
@@ -1,10 +1,4 @@
 require 'test_helper'
 
 class TasksIntegrationTest < ActionDispatch::IntegrationTest
-  fixtures :all
-
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
 end
