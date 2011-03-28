fa3e8eed441ec91a82360db9a90fe065a7b0ddeb
Michael Raidel <raidel@induktiv.at>
2011-03-28 12:53:56 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PomodoroTimer::Application.config.secret_token = 'cbc07fcd849ee77a6f78a97aaf675844fee12c61fa8d14115280e7f852f921f7b20d36aa07a804f8fa1bd0eb1669f4eb02f811a1467089ca7e7ec31ca53742c3'
