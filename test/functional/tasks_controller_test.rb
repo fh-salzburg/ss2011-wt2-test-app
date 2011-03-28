4ac762d498da2e2649c516b25721905540cfd08b
Michael Raidel <raidel@induktiv.at>
2011-03-28 13:19:24 +0200

    test for real content

--- test/functional/tasks_controller_test.rb
+++ test/functional/tasks_controller_test.rb
@@ -1,8 +1,18 @@
 require 'test_helper'
 
 class TasksControllerTest < ActionController::TestCase
-  test "index" do
+  should "get :index" do
+    @task_1 = Task.make
+    @task_2 = Task.make
+
     get :index
     assert_response :success
+
+    # assert_same_elements ist eine Shoulda-Assertion
+    # sie vergleicht, ob die selben Elemente enthalten sind
+    # dabei kommt es im Gegensatz zu assert_equal nicht auf die Reihenfolge an
+    assert_same_elements [ @task_1, @task_2 ], assigns(:tasks)
+
+    assert_select "ul#tasks li", 2
   end
 end
--- test/integration/tasks_integration_test.rb
+++ test/integration/tasks_integration_test.rb
@@ -1,8 +1,15 @@
 require 'test_helper'
 
 class TasksIntegrationTest < ActionDispatch::IntegrationTest
-  test "show tasks" do
+  should "show tasks" do
+    Task.make(:title => "first task")
+    Task.make(:title => "second task")
     get '/tasks'
     assert_response :success
+    # checking the html structure
+    assert_select 'ul#tasks li', 2
+    # or just checking the content
+    assert_match "first task", response.body
+    assert_match "second task", response.body
   end
 end
