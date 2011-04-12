6b5d2fef90dbf89599d7f0cbbc6672cb346330f5
Michael Raidel <raidel@induktiv.at>
2011-04-12 10:35:43 +0200

    unit test

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ActiveRecord::Base
+  validates :title, :presence => true
+  validates :estimated_length, :numericality => { :only_integer => true, :greater_than => 0, :less_than => 6 }
 end
--- test/unit/task_test.rb
+++ test/unit/task_test.rb
@@ -1,8 +1,12 @@
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  should validate_presence_of(:title)
+
+  should "validate inclusion of" do
+    assert !Task.make_unsaved(:estimated_length => 6).valid?
+    assert !Task.make_unsaved(:estimated_length => 0).valid?
+    assert_valid Task.make_unsaved(:estimated_length => 1)
+    assert_valid Task.make_unsaved(:estimated_length => 4)
   end
 end
